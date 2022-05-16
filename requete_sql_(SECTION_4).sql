0c04e431ecba4670f0ce65e0355ce24a61faafc0
= <=>
2022-05-16 01:40:04 -0400

    DataEngineer

--- requete_sql_(SECTION_4).sql
+++ requete_sql_(SECTION_4).sql
@@ -1,17 +1,14 @@
-SET @NB_MAX_LOC = (SELECT num FROM (
-            SELECT customer_id, COUNT(*) AS num FROM rental GROUP BY customer_id ORDER BY num DESC LIMIT 1
-        ) AS T
-);
-
 SET @ID_CLIENT_MAX_LOC = (SELECT customer_id FROM (
             SELECT customer_id, COUNT(*) AS num FROM rental GROUP BY customer_id ORDER BY num DESC LIMIT 1
         ) AS T
 );
 
-SELECT c.first_name, c.last_name, a.address, @NB_MAX_LOC as nb_locations, a.postal_code, a.city,a.latitude,a.longitude
+SELECT c.last_name, c.first_name,  a.address, count(r.customer_id) as Max, a.postal_code, a.city,a.latitude,a.longitude
 
 FROM customer c
 INNER JOIN address a
 on c.address_id = a.address_id
+INNER JOIN rental r
+on c.customer_id = r.customer_id
 
 WHERE c.customer_id =  @ID_CLIENT_MAX_LOC;
